99576b6380759f36b202ed9ba37afb031fb8a3fd
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-05-13 00:32:41 +0900

    アセットパイプラインの設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # While tests run files are not watched, reloading is not necessary.
+  config.assets.compile = true
+  config.assets.debug = false
   config.enable_reloading = false
 
   # Eager loading loads your entire application. When running a single test locally,
