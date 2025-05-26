005e630838d7c65185ce2d47e1a778e417a0a405
takashi saito <takashisaito@MacBook-Air-ts.local>
2025-05-26 17:24:03 +0900

    Lintチェック

--- db/migrate/20250526074906_add_omniauth_to_users.rb
+++ db/migrate/20250526074906_add_omniauth_to_users.rb
@@ -2,7 +2,7 @@ class AddOmniauthToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :provider, :string unless column_exists?(:users, :provider)
     add_column :users, :uid, :string unless column_exists?(:users, :uid)
-    
-    add_index :users, [:provider, :uid], unique: true unless index_exists?(:users, [:provider, :uid])
+
+    add_index :users, [ :provider, :uid ], unique: true unless index_exists?(:users, [ :provider, :uid ])
   end
 end
